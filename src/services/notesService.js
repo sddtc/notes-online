3d6a882383c25603c49b7f4df057fc2f341cdb69
sddtc <changhbaga@gmail.com>
2025-06-08 22:21:44 +0800

    feature: add service unit tests and eslint

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -4,25 +4,25 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_DB_HOST
 const supabaseKey = import.meta.env.VITE_DB_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+const notesOnlineTable = 'notes-online';
 
 async function getNotes() {
-    const { data, error } = await supabase.from("notes-online").select('note');
+    const { data, error } = await supabase.from(notesOnlineTable).select('note');
+
+    if (error) {
+        return []
+    }
 
     return data.map(d => d.note);
 }
 
 async function putNote(newNote) {
     const record = { note: newNote, creator: "anonymous" }
-    await supabase.from("notes-online").insert(record, { returning: 'minimal' });
-
-    console.log("put note successfully.")
+    await supabase.from(notesOnlineTable).insert(record, { returning: 'minimal' });
 }
 
 async function deleteNote(note) {
-    const response = await supabase
-        .from('notes-online')
-        .delete()
-        .eq('note', note)
+    await supabase.from(notesOnlineTable).delete().eq('note', note);
 }
 
 export default { getNotes, putNote, deleteNote };
---
+++ test/notesService.test.js
@@ -0,0 +1,68 @@
+import { beforeEach, describe, it, vi, expect } from "vitest";
+import notesService from "../src/services/notesService";
+
+const mockSelectFunction = vi.fn();
+const mockInsertFunction = vi.fn();
+const mockDeleteFunction = vi.fn();
+const mockEqFunction = vi.fn();
+
+const { supabaseMockClient } = vi.hoisted(() => {
+    return {
+        supabaseMockClient: {
+            from: vi.fn(() => ({
+                select: mockSelectFunction,
+                insert: mockInsertFunction,
+                delete: vi.fn(() => ({
+                    eq: mockEqFunction
+                })),
+            }))
+        }
+    }
+})
+
+vi.mock('@supabase/supabase-js', () => ({
+    createClient: vi.fn(() => supabaseMockClient),
+}));
+
+describe("notes service layer tests", () => {
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it("should return notes array", async () => {
+        mockSelectFunction.mockResolvedValue({ data: [{ note: 'note1' }, { note: 'note2' }], error: null })
+
+        const result = await notesService.getNotes();
+
+        expect(supabaseMockClient.from).toHaveBeenCalledWith('notes-online');
+        expect(supabaseMockClient.from().select).toHaveBeenCalledWith('note');
+        expect(result).toEqual(['note1', 'note2']);
+    });
+
+    it("should return empty array when return error", async () => {
+        mockSelectFunction.mockResolvedValue({ data: null, error: "some errors" });
+
+        const result = await notesService.getNotes();
+
+        expect(result).toEqual([]);
+    });
+
+    it("should save note successfully", async () => {
+        mockInsertFunction.mockResolvedValue();
+
+        await notesService.putNote("new fake note");
+
+        expect(supabaseMockClient.from).toHaveBeenCalledWith('notes-online');
+        expect(supabaseMockClient.from().insert).toHaveBeenCalledWith({ note: "new fake note", creator: "anonymous" }, { returning: 'minimal' });
+    });
+
+    it("should delete note successfully", async () => {
+        mockDeleteFunction.mockResolvedValue();
+        mockEqFunction.mockResolvedValue();
+
+        await notesService.deleteNote("fake note");
+
+        expect(supabaseMockClient.from().delete().eq).toHaveBeenCalledWith('note', 'fake note');
+    })
+});
