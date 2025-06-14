614ff23494c3bea00b00fcb7d18197313ec17b35
sddtc <changhbaga@gmail.com>
2025-06-15 00:21:57 +0800

    chore: update eslint files

--- test/components/Note.test.jsx
+++ test/components/Note.test.jsx
@@ -1,11 +1,23 @@
-import { describe, it, expect, vi } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { describe, it, expect, vi, beforeEach } from "vitest"
+import { render, screen, fireEvent } from "@testing-library/react"
 import Note from "../../src/components/Note"
 
 describe("handle note component", () => {
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
 
     it("should render note content successfully", () => {
         render(<Note content="fake content" />)
         expect(screen.getByText("fake content")).toBeInTheDocument();
     });
+
+    it("should called delete func when click the delete button", () => {
+        const mockDeleteFunc = vi.fn();
+
+        render(<Note onDelete={mockDeleteFunc} id="fake-1" />);
+        fireEvent.click(screen.getByRole("button"));
+        expect(mockDeleteFunc).toHaveBeenCalledTimes(1)
+        expect(mockDeleteFunc).toHaveBeenCalledWith("fake-1")
+    })
 })
